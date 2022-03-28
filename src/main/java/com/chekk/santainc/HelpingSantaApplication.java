1bec61853584df7e469b027463a7abbf0d609760
Pulkit Pradeep Gupta <pulkit@reloy.co>
2022-03-28 18:45:47 +0530

    First commit

---
+++ src/main/java/com/chekk/santainc/HelpingSantaApplication.java
@@ -0,0 +1,13 @@
+package com.chekk.santainc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingSantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpingSantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chekk/santainc/HelpingSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chekk.santainc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpingSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
